0ac381812417fcb2198a87ff31b1c3839ac8144f
dev-yakuza <dev.yakuza@gmail.com>
2023-02-21 20:03:18 +0900

    Add multi variables linear regression

--- keras_model/multi_variables_linear_regression.py
+++ keras_model/multi_variables_linear_regression.py
@@ -4,24 +4,66 @@ from tensorflow.keras.optimizers import SGD
 import matplotlib.pyplot as plt
 import numpy as np
 
-x_data = np.array([1, 2, 3, 4, 5, 6])
-y_data = np.array([3, 4, 5, 6, 7, 8])
+x_data = np.array([
+    [1, 2, 0],
+    [5, 4, 3],
+    [1, 2, -1],
+    [3, 1, 0],
+    [2, 4, 2],
+    [4, 1, 2],
+    [-1, 3, 2],
+    [4, 3, 3],
+    [0, 2, 6],
+    [2, 2, 1],
+    [1, -2, -2],
+    [0, 1, 3],
+    [1, 1, 3],
+    [0, 1, 4],
+    [2, 3, 3],
+])
+y_data = np.array([
+    [-4],
+    [4],
+    [-6],
+    [3],
+    [-4],
+    [9],
+    [-7],
+    [5],
+    [6],
+    [0],
+    [4],
+    [3],
+    [5],
+    [5],
+    [1]
+])
 
 print('Data shape ======================================================')
 print(f'x_data.shape: {x_data.shape}')
 print(f'y_data.shape: {y_data.shape}')
 
 model = Sequential()
-model.add(Flatten(input_shape=(1,)))
+model.add(Flatten(input_shape=(3,)))
 model.add(Dense(1, activation='linear'))
+# model.add(Dense(1, input_shape=(3,), activation='linear'))
 
 model.compile(optimizer=SGD(learning_rate=1e-2), loss='mse')
 model.summary()
 
 hist = model.fit(x_data, y_data, epochs=1000, verbose=0)
 
-predict_data = np.array([-3.1, 3.0, 3.5, 15.0, 20.1])
-result_data = np.array([-1.1, 5.0, 5.5, 17.0, 22.1])
+predict_data = np.array([
+    [5, 5, 0],
+    [2, 3, 1],
+    [-1, 0, -1],
+    [-1, 0, -1],
+    [10, 5, 2],
+    [4, -1, -2],
+])
+result_data = [
+    2 * data[0] - 3 * data[1] + 2 * data[2] for data in predict_data
+]
 result = model.predict(predict_data)
 
 print('Result ==========================================================')
